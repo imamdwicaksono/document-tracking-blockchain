8eaf1dca5bcfb5dfd050d6e5b5532e992b00a16e
Imam Dwicaksono <imam.dwicaksono@mmsgroup.co.id>
2025-08-11 10:11:37 +0700

    Update cleanup script to stop specific services and update subproject commits for doc-tracker-api and doc-tracker-ui

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Stopping services..."
-kill 47349 2>/dev/null
-kill 47351 2>/dev/null
-kill 47355 2>/dev/null
+kill 11820 2>/dev/null
+kill 11822 2>/dev/null
+kill 11827 2>/dev/null
 echo "Services stopped"
